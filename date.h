5db0c3615724686565b3eebf10d4df385375cf3a
Alexey <91151767+Azriel230@users.noreply.github.com>
2023-02-05 15:45:52 +0300

    Update date.h

--- date.h
+++ date.h
@@ -8,9 +8,28 @@ class Date
 
 public:
 
+	bool IsLeapYear() const;
+	Date MinDate(Date obj1_, Date obj2_) const;
+	Date MaxDate(Date obj1_, Date obj2_) const;
+
+
 	Date(int day_, int month_, int year_);
+	Date(const Date& obj_);
+
+	~Date() {}
+
+	Date& operator=(const Date& obj_);
+	bool operator==(const Date& obj_);
+	bool operator!=(const Date& obj_);
 
 	int GetDay() const;
 	int GetMonth() const;
 	int GetYear() const;
+
+	Date operator+ (int days_);
+	Date operator- (int days_);
+
+	int operator- (Date obj_);
+
+	friend std::ostream& operator<<(std::ostream& stream, const Date& obj);
 };
