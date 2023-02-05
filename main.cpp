e9031643e30002fe04d845ece52d1125a3abd244
Alexey <91151767+Azriel230@users.noreply.github.com>
2023-02-05 15:45:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 
 int main()
 {
-	Date date(2, 6, 2003);
+	Date date(1, 1, 2020);
 
+	//for(int i = 100; i <= 1000; i+=100)
+	//{
+	//	Date newDate = date - i;
+	//	std::cout << newDate << std::endl;
+	//}
+
+	Date d1(01, 01, 2020);
+	Date d2(2, 6, 2023);
+
+	std::cout << d1 - d2;
+	
 	return 0;
 }
